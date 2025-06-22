024528833f1633f3d8f4a93d4bb2377deb558b5e
ahmed elhefny <103605461+AhElhefny@users.noreply.github.com>
2025-06-22 10:06:45 +0300

    refact: refactor the api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['controller' => TaskController::class,'prefix' => 'tasks'],function(){
+    Route::post('/', 'store')->middleware('throttle:task-creation');
+    Route::patch('{task}/update-status', 'updateStatus'); 
+});
 Route::apiResource('tasks', TaskController::class)->except(['store']);
-Route::post('/tasks', [TaskController::class, 'store'])->middleware('throttle:task-creation');
-Route::patch('tasks/{task}/update-status', [TaskController::class, 'updateStatus']);
+
